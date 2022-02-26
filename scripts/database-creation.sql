83442a0a494f2a291e0aa2035e537627dbf98397
Ismael Carlos Velten <ismaelcarlosvelten@gmail.com>
2022-02-26 17:06:36 -0300

    Updating database schema for servers

--- scripts/database-creation.sql
+++ scripts/database-creation.sql
@@ -4,17 +4,35 @@ create schema discord;
 create domain discord.id varchar(25);
 create domain discord.message varchar(2000);
 create domain public.nanoid varchar(21);
-create type public.bibleversion as enum ('acf', 'apee', 'bbe', 'kjv', 'nvi', 'ra', 'rvr');
+
+create type public.bible_version as enum (
+	'acf', 'apee', 'bbe', 'kjv', 'nvi', 'ra', 'rvr'
+);
+
+create type public.culture_info as enum (
+	'pt-BR', 'en-US'
+);
+
+create type discord.prayer_petition_setting as (
+	approval_channel_id discord.id,
+	petition_channel_id discord.id
+);
 
 create table discord.user_settings (
 	user_id discord.id not null primary key,
-	default_bible_version public.bibleversion
+	default_bible_version public.bible_version
+);
+
+create table discord.server_settings (
+	server_id discord.id not null primary key,
+	default_bible_version public.bible_version,
+	prayer_petition_setting discord.prayer_petition_setting,
+	culture public.culture_info not null default 'pt-BR'
 );
 
 create table discord.prayer_petitions (
 	prayer_id public.nanoid not null primary key,
-	petition_date_time timestamp not null,
+	petition_date date not null,
 	user_id discord.id,
-	approved bit,
 	message discord.message not null
 );
